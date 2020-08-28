dfba17ecda5754d17fd98f7c4fb00b683a943286
yuwatida <yuwatida@yuwatidas-MacBook-Pro.local>
2020-08-28 16:18:04 -0400

    add Cucumber Test

---
+++ Driver.java
@@ -0,0 +1,61 @@
+package utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+public class Driver {
+    private static String propertyPath = "src/test/resources/conf/configuration.properties";
+    private static WebDriver driver;
+
+    private static final String sauceUser = ConfigReader.readProperty(propertyPath, "sauceUsername");
+    private static final String sauceKey = ConfigReader.readProperty(propertyPath,"sauceKey");
+    private static final String URL = "https://" + sauceUser + ":" + sauceKey + "@ondemand.us-west-1.saucelabs.com:443/wd/hub";
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            if(ConfigReader.readProperty(propertyPath,"executeTestLocally=false").equalsIgnoreCase("false")) {
+                driver = getRemoteDriver();
+            }else{
+            switch (ConfigReader.readProperty("browser", propertyPath)) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                default:
+                    System.out.println("Invalid browser type");
+            }
+            }
+        }
+        return driver;
+    }
+
+    public static void quitDriver(){
+        getDriver().quit();
+        driver = null;
+    }
+    public static WebDriver getRemoteDriver(){
+        WebDriver driver = null;
+        try{
+            DesiredCapabilities capabilitie = DesiredCapabilities.chrome();
+            capabilitie.setCapability("browserName","chrome");
+            capabilitie.setCapability("version", ConfigReader.readProperty(propertyPath, "browser_version"));
+            capabilitie.setCapability("platform", ConfigReader.readProperty(propertyPath, "os"));
+            driver = new RemoteWebDriver(new URL(URL),capabilitie);
+        }catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+        return driver;
+    }
+}
